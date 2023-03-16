fe3ac276df670ed29cc70940408e5907db54da04
caxapexac <caxapexac@gmail.com>
2023-03-17 03:20:54 +0400

    Removed legacy prrojects

--- ShreddedWaterUnityProject3FromSS2/Assets/Starstorm2/Modules/Buffs/BuffTypes/ExecutionerSuperCharged.cs
+++
@@ -1,9 +0,0 @@
-﻿using RoR2;
-
-namespace Moonstorm.Starstorm2.Buffs
-{
-    public sealed class ExecutionerSuperCharged : BuffBase
-    {
-        public override BuffDef BuffDef { get; } = SS2Assets.LoadAsset<BuffDef>("BuffExecutionerSuperCharged", SS2Bundle.Executioner);
-    }
-}
--- ShreddedWaterUnityProjectFromSS2/Assets/ShreddedWater/Scripts/Core/ContentLoaders/CommonContentLoader.cs
+++
@@ -1,9 +0,0 @@
-﻿using Moonstorm.Loaders;
-
-
-namespace ShreddedWater
-{
-    public abstract class CommonContentLoader<T> : ContentLoader<T> where T : CommonContentLoader<T>
-    {
-    }
-}
