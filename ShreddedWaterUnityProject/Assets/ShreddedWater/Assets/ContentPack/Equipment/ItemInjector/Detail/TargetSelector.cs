04041e4df96168aed3f1edae5ce70525e7aef811
Destructor <destrovi4@gmail.com>
2023-03-19 19:00:40 +0300

    TergetSelection

---
+++ ShreddedWaterUnityProject/Assets/ShreddedWater/Assets/ContentPack/Equipment/ItemInjector/Detail/TargetSelector.cs
@@ -0,0 +1,122 @@
+﻿using Moonstorm.Starstorm2;
+using RoR2;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSelector : MonoBehaviour
+{
+    private GameObject _selection;
+
+    public float UpdateInterval = 0.5f;
+    private float _updateDelay = 0.0f;
+
+    public float Radius = 100.0f;
+    public Ray Ray;
+    public LayerMask LayerMask = Physics.AllLayers;
+    public QueryTriggerInteraction QueryTriggerInteraction = QueryTriggerInteraction.UseGlobal;
+
+    public CharacterBody Selected;
+    public TeamMask TeamMask;
+
+    private void Awake()
+    {
+        _selection = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        _selection.transform.SetParent(transform, false);
+        _selection.transform.localPosition = Vector3.zero;
+        foreach (Collider component in _selection.GetComponents<Collider>())
+        {
+            Destroy(component);
+        }
+        foreach (Rigidbody component in _selection.GetComponents<Rigidbody>())
+        {
+            Destroy(component);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(_selection);
+    }
+
+    private void Update()
+    {
+        if (Selected != null)
+        {
+            _selection.transform.position = Selected.transform.position;
+        }
+        else
+        {
+            _selection.transform.position = transform.position;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _updateDelay -= Time.fixedDeltaTime;
+        if (_updateDelay > 0)
+            return;
+        _updateDelay = UpdateInterval;
+        Selected = Select();
+
+        SS2Log.Warning($"{nameof(TargetSelector)}.{nameof(FixedUpdate)} {(Selected == null ? "none" : Selected.name)}");
+    }
+
+    private CharacterBody Select()
+    {
+        Collider[] array = Physics.OverlapSphere(Ray.origin, Radius, LayerMask, QueryTriggerInteraction);
+        if (array.Length == 0)
+            return null;
+
+        for(int index = 0; index < array.Length; index++)
+        {
+            CharacterBody bestBody = Validate(array[index]);
+            if (bestBody == null)
+                continue;
+
+            float bestSerchScore = Estimate(bestBody);
+
+            for (; index < array.Length; index++)
+            {
+                CharacterBody body = Validate(array[index]);
+                if (body == null)
+                    continue;
+
+                float serchScore = Estimate(body);
+                if (serchScore < bestSerchScore)
+                    continue;
+                bestBody = body;
+                bestSerchScore = serchScore;
+            }
+
+            return bestBody;
+        }
+
+        return null;
+    }
+
+    private CharacterBody Validate(Collider collider)
+    {
+        CharacterBody body = collider.GetComponentInParent<CharacterBody>();
+        if(body == null)
+            return null;
+
+        Vector3 localPosition = collider.transform.position - Ray.origin;
+        float distance = Vector3.Dot(Ray.direction, localPosition);
+        if (distance < 0)
+            return null;
+
+        if (!TeamMask.HasTeam(body.teamComponent.teamIndex))
+            return null;
+
+        return body;
+    }
+
+    private float Estimate(CharacterBody body)
+    {
+        Vector3 localPosition = body.transform.position - Ray.origin;
+        float distance = Vector3.Dot(Ray.direction, localPosition);
+        Vector3 projection = Ray.GetPoint(distance);
+        return distance + (localPosition - projection).sqrMagnitude / (distance);
+    }
+}
--- ShreddedWaterUnityProject/Assets/ShreddedWater/Scripts/Modules/Equipment/ItemInjector.cs
+++ ShreddedWaterUnityProject/Assets/ShreddedWater/Scripts/Modules/Equipment/ItemInjector.cs
@@ -10,17 +10,62 @@ namespace ShreddedWater.Items
 {
     public class ItemInjector : EquipmentBase
     {
-        private const string token = "SS2_EQUIP_ITEMINJECTOR_DESC";
+        private const string token = "SW_EQUIP_ITEMINJECTOR_DESC";
         public override EquipmentDef EquipmentDef { get; } = SWAssetsLoader.Instance.LoadAsset<EquipmentDef>("ItemInjector", SWBundleEnum.Main);
 
-        //SS2Log.Warning($"Spawned debug item {name}");
-
+        private Behavior _behavior;
+        
+        public override void AddBehavior(ref CharacterBody body, int stack)
+        {
+            _behavior = body.AddItemBehavior<Behavior>(stack);
+        }
 
         public override bool FireAction(EquipmentSlot slot)
         {
-            SS2Log.Info($"ItemInjector activated");
+            return _behavior.FireAction(slot);
+        }
 
-            return true;
+        public sealed class Behavior : CharacterBody.ItemBehavior
+        {
+            private TargetSelector _targetSelector;
+            private InputBankTest _inputBank;
+
+            private void Start()
+            {
+
+                Chat.SendBroadcastChat(new Chat.SimpleChatMessage { baseToken = $"{nameof(ItemInjector)}.{nameof(Start)}" });
+                _targetSelector = body.gameObject.AddComponent<TargetSelector>();
+                _targetSelector.TeamMask = TeamMask.GetEnemyTeams(body.teamComponent.teamIndex);
+                _inputBank = body.GetComponent<InputBankTest>();
+            }
+
+            private void FixedUpdate()
+            {
+                if (_inputBank != null)
+                {
+                    _targetSelector.Ray = new Ray(_inputBank.aimOrigin, _inputBank.aimDirection);
+                }
+                else
+                {
+                    _targetSelector.Ray = new Ray(body.corePosition, body.characterDirection.forward); // Not checked
+                }
+            }
+
+            private void OnDestroy()
+            {
+                Destroy(_targetSelector);
+            }
+
+            public bool FireAction(EquipmentSlot slot)
+            {
+                CharacterBody body = _targetSelector.Selected;
+                if(body == null)
+                    return false;
+
+                //Chat.SendBroadcastChat(new Chat.SimpleChatMessage { baseToken = $"{nameof(ItemInjector)}.{nameof(FireAction)}" });
+                //SS2Log.Warning($"{nameof(ItemInjector)}.{nameof(FireAction)}");
+                return true;
+            }
         }
     }
 }
